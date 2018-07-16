9e0057ee7098aa6db3a014c291cf7d8d35c86d86
lian <imwhatiam123@gmail.com>
2018-07-16 10:54:55 +0800

    [alibaba custom] read alibaba message for leave_file_handover
    reshare repo to users/group

--- seahub/base/management/commands/read_alibaba_message.py
+++ seahub/base/management/commands/read_alibaba_message.py
@@ -12,6 +12,7 @@ from seaserv import seafile_api
 
 from seahub.alibaba.models import AlibabaMessageQueue, AlibabaProfile, \
         ALIBABA_MESSAGE_TOPIC_LEAVE_FILE_HANDOVER
+from seahub.group.utils import is_group_member
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -58,8 +59,48 @@ class Command(BaseCommand):
                     leave_ccnet_email, ret_corrupted=False)
 
             for repo in leave_owned_repos:
+
                 if seafile_api.repo_has_been_shared(repo.id, including_groups=True):
+
+                    # get repo shared to user/group/public list
+                    shared_users = seafile_api.list_repo_shared_to(
+                            leave_ccnet_email, repo.id)
+                    shared_groups = seafile_api.list_repo_shared_group_by_user(
+                            leave_ccnet_email, repo.id)
+                    pub_repos = seafile_api.list_inner_pub_repos_by_owner(leave_ccnet_email)
+
+                    # transfer repo
                     seafile_api.set_repo_owner(repo.id, super_ccnet_email)
+
+                    # reshare repo to user
+                    for shared_user in shared_users:
+
+                        shared_username = shared_user.user
+                        if super_ccnet_email== shared_username:
+                            continue
+
+                        seafile_api.share_repo(repo.id, super_ccnet_email,
+                                shared_username, shared_user.perm)
+
+                    # reshare repo to group
+                    for shared_group in shared_groups:
+
+                        shared_group_id = shared_group.group_id
+                        if not is_group_member(shared_group_id, super_ccnet_email):
+                            continue
+
+                        seafile_api.set_group_repo(repo.id, shared_group_id,
+                                super_ccnet_email, shared_group.perm)
+
+                    # check if current repo is pub-repo
+                    # if YES, reshare current repo to public
+                    for pub_repo in pub_repos:
+
+                        if repo.id != pub_repo.id:
+                            continue
+
+                        seafile_api.add_inner_pub_repo(repo.id, pub_repo.permission)
+                        break
                 else:
                     seafile_api.remove_repo(repo.id)
 
