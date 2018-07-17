85337126b2227fd8301640e6c46b4256a5240893
lian <imwhatiam123@gmail.com>
2018-07-17 19:00:41 +0800

    [alibaba custom] update download.html
    add settings for en version

--- seahub/alibaba/views.py
+++ seahub/alibaba/views.py
@@ -9,12 +9,19 @@ from seahub.auth.decorators import login_required
 try:
     from seahub.settings import WINDOWS_CLIENT_PUBLIC_DOWNLOAD_URL, \
             WINDOWS_CLIENT_VERSION, APPLE_CLIENT_PUBLIC_DOWNLOAD_URL, \
-            APPLE_CLIENT_VERSION
+            APPLE_CLIENT_VERSION, WINDOWS_CLIENT_PUBLIC_DOWNLOAD_URL_EN, \
+            WINDOWS_CLIENT_VERSION_EN, APPLE_CLIENT_PUBLIC_DOWNLOAD_URL_EN, \
+            APPLE_CLIENT_VERSION_EN
+
 except ImportError:
     WINDOWS_CLIENT_PUBLIC_DOWNLOAD_URL = ''
     WINDOWS_CLIENT_VERSION = ''
     APPLE_CLIENT_PUBLIC_DOWNLOAD_URL = ''
     APPLE_CLIENT_VERSION = ''
+    WINDOWS_CLIENT_PUBLIC_DOWNLOAD_URL_EN = ''
+    WINDOWS_CLIENT_VERSION_EN = ''
+    APPLE_CLIENT_PUBLIC_DOWNLOAD_URL_EN = ''
+    APPLE_CLIENT_VERSION_EN = ''
 
 @login_required
 def alibaba_client_download_view(request):
@@ -24,4 +31,8 @@ def alibaba_client_download_view(request):
             'windows_client_version': WINDOWS_CLIENT_VERSION,
             'apple_client_public_download_url': APPLE_CLIENT_PUBLIC_DOWNLOAD_URL,
             'apple_client_version': APPLE_CLIENT_VERSION,
+            'windows_client_public_download_url_en': WINDOWS_CLIENT_PUBLIC_DOWNLOAD_URL_EN,
+            'windows_client_version_en': WINDOWS_CLIENT_VERSION_EN,
+            'apple_client_public_download_url_en': APPLE_CLIENT_PUBLIC_DOWNLOAD_URL_EN,
+            'apple_client_version_en': APPLE_CLIENT_VERSION_EN,
         })
